350ceee8f38b31f6c17161e9bd0b657484f18b93
MadhavDodiya <madhavdodiya2017@gmail.com>
2024-04-03 13:42:29 +0530

    at classes

--- src/App.js
+++ src/App.js
@@ -260,21 +260,53 @@ function App() {
     <div className='container-fluid mt-5'>
       <div className='container'>
         <div className='row'>
-          <div className='col-md-6 bg-warning'>
+          <div className='col-md-6'>
             <div className='row'>
-              <div className='col-md-12 bg-danger'>
+              <div className='col-md-12'>
                 <div className='row'>
-                <div className='col-md-6 bg-success p-0'>
-                  <img src='img/hotel-single-room.jpg' className='img-fluid'/>
+                <div className='col-md-6 p-0'>
+                  <img src='img/hotel-single-room.jpg' style={{height:"250px",width:"100%"}}/>
                   <div className="container-fluid p-2" style={{background: "#11202a"}}>
                     <span className='clrw'>$ 125 / Night</span>
                   </div>
                 </div>
-                <div className='col-md-6 bg-primary'>
-                  <div className='container p-3'>
-                    <h1 className='p-2'>Single Room</h1>
-                    <h4 className='p-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+                <div className='col-md-6'>
+                  <div className='container pt-4'>
+                    <h3>Single Room</h3>
+                    <h5 className='pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+                    <div className='container-fluid pt-3'>
+                      <img src='img/beer.svg' style={{height:"50px"}}/>
+                      <img src='img/car-rental.svg' style={{height:"50px"}}/>
+                      <img src='img/coffee.svg' style={{height:"50px"}}/>
+                      <img src='img/hair-dryer.svg' style={{height:"50px"}}/>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
+          <div className='col-md-6'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <div className='row'>
+                <div className='col-md-6 p-0'>
+                  <img src='img/executive-suite-free-img.jpg' style={{height:"250px",width:"100%"}}/>
+                  <div className="container-fluid p-2" style={{background: "#11202a"}}>
+                    <span className='clrw'>$ 125 / Night</span>
+                  </div>
+                </div>
+                <div className='col-md-6'>
+                  <div className='container pt-4'>
+                    <h3>Executive Suite</h3>
+                    <h5 className='pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+                    <div className='container-fluid pt-3'>
+                      <img src='img/beer.svg' style={{height:"50px"}}/>
+                      <img src='img/car-rental.svg' style={{height:"50px"}}/>
+                      <img src='img/coffee.svg' style={{height:"50px"}}/>
+                      <img src='img/hair-dryer.svg' style={{height:"50px"}}/>
+                    </div>
                   </div>
                 </div>
                 </div>
@@ -284,6 +316,70 @@ function App() {
         </div>
       </div>
     </div>
+
+    <div className='container-fluid mt-5'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <div className='row'>
+                <div className='col-md-6 p-0'>
+                  <img src='img/double-room-free-img.jpg' style={{height:"250px",width:"100%"}}/>
+                  <div className="container-fluid p-2" style={{background: "#11202a"}}>
+                    <span className='clrw'>$ 125 / Night</span>
+                  </div>
+                </div>
+                <div className='col-md-6'>
+                  <div className='container pt-4'>
+                    <h3>Double Room</h3>
+                    <h5 className='pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+                    <div className='container-fluid pt-3'>
+                      <img src='img/beer.svg' style={{height:"50px"}}/>
+                      <img src='img/car-rental.svg' style={{height:"50px"}}/>
+                      <img src='img/coffee.svg' style={{height:"50px"}}/>
+                      <img src='img/hair-dryer.svg' style={{height:"50px"}}/>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-md-6'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <div className='row'>
+                <div className='col-md-6 p-0'>
+                  <img src='img/executive-suite-free-img.jpg' style={{height:"250px",width:"100%"}}/>
+                  <div className="container-fluid p-2" style={{background: "#11202a"}}>
+                    <span className='clrw'>$ 125 / Night</span>
+                  </div>
+                </div>
+                <div className='col-md-6'>
+                  <div className='container pt-4'>
+                    <h3>Duplex Suite</h3>
+                    <h5 className='pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+                    <div className='container-fluid pt-3'>
+                      <img src='img/beer.svg' style={{height:"50px"}}/>
+                      <img src='img/car-rental.svg' style={{height:"50px"}}/>
+                      <img src='img/coffee.svg' style={{height:"50px"}}/>
+                      <img src='img/hair-dryer.svg' style={{height:"50px"}}/>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* package end */}
+
+    {/* form start */}
+    <div className=''></div>
 
 
 
